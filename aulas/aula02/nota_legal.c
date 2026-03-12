c23bd36851772e3ae99ade6103554f813fef47ac
João Paulo <joaopaulomaslow@gmail.com>
2026-03-11 21:57:40 -0300

    adiciona codigos da aula02

---
+++ aulas/aula02/nota_legal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+
+    printf("=========================\n");
+
+    printf("  NOTA  LEGAL\n");
+
+    printf("========================\n");
+
+    printf("Produto Qtd Valor (R$)\n");
+
+    printf("Banana   12        50,00\n");
+
+    printf("Laranja  06         25,00\n");
+
+    printf("Abacaxi  01         30,00\n");
+
+    printf("-------------------------\n");
+
+    printf("TOTAL               105,00\n");
+
+    return 0;
+}
---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main () { 
+    printf("10,-5,00004");
+    return 0;
+}
